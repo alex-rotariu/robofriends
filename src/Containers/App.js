4b2c8a6d6f7758bf5b6c827605be18cca6c4e440
Alex Rotariu <49029073+alex-rotariu@users.noreply.github.com>
2020-02-21 19:26:46 +0200

    robofriends

---
+++ src/Containers/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import CardList from '../Components/CardList';
+import Scroll from '../Components/Scroll'
+import SearchBox from '../Components/SearchBox';
+import './App.css';
+import ErrorBoundry from '../Components/ErrorBoundry';
+
+class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => {this.setState({robots: users})});
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value})
+	}
+ 	render(){
+ 		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		});
+		if(this.state.robots.length === 0){
+			return <h1>Loading</h1>;
+		} 
+		else 
+		{ 
+	 		return (
+				<div className='tc'>
+					<h1 className='f2'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<Scroll>
+						<ErrorBoundry>
+							<CardList robots={filteredRobots}/>
+						</ErrorBoundry>
+					</Scroll>
+				</div>
+			);
+ 		}
+ 	}
+}
+
+export default App;
